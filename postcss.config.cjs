9d29f88aa8dab90026dc041ec734ea0e966b8946
josehernandezv <jose.andres.art@gmail.com>
2023-09-19 09:54:13 -0600

    Replace tailwind with open-props

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+const postcssJitProps = require("postcss-jit-props");
+const OpenProps = require("open-props");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  plugins: [postcssJitProps(OpenProps)],
+  autoprefixer,
+};
