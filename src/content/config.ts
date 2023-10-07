97d9a0a9b25f6d964953bc95d11122d1fdc3aa8e
josehernandezv <jose.andres.art@gmail.com>
2023-10-06 19:20:28 -0600

    Projects WIP

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,14 +7,31 @@ const blogCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      // tags: z.array(z.string()),
       cover: image(),
       coverAlt: z.string(),
       theme: z.string().optional(),
+    }),
+});
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      excerpt: z.string(),
+      cover: image(),
+      coverAlt: z.string(),
+      theme: z.string().optional(),
+      createdAt: z.date(),
+      updatedAt: z.date(),
+      url: z.string().optional(),
+      repository: z.string().optional(),
+      type: z.enum(["personal", "tutorial"]).default("personal"),
+      tags: z.array(z.string()),
     }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   blog: blogCollection,
+  projects: projectsCollection,
 };
