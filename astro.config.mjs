715ca646fe72adca1dcd2968a087e344db5cb5bf
josehernandezv <jose.andres.art@gmail.com>
2024-06-10 17:14:43 -0600

    Upgrade to Astro v4 + upgrade all deps ⬆️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), icon()],
 });
